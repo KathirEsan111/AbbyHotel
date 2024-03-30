4c42985c1976d60ffc80c1f5edcde24b00b230aa
KathirEsan111 <alskadhir@gmail.com>
2024-03-31 00:05:20 +0530

    Till Ajax in menuitems.js

---
+++ AbbyWeb/wwwroot/js/menuItem.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+    $('#DT_Load').DataTable({
+        "ajax": {
+            "url": "/api/MenuItem",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "name", "width": "25%" },
+            { "data": "price", "width": "15%" },
+        ], "width":"100%"
+
+    });
+});
